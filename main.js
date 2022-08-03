14440be6264097f9527fa937d2085b92151ef8a8
Billy Phannavong <billy.phannavong@gmail.com>
2022-08-03 12:01:46 -0700

    Add basic html skeleton and gameboard

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+// Gameboard object module
+const gameBoard = (() => {
 
+    const tiles = ['x', 'o', 'x'];
+
+})();
+
+// Module object to control game flow
+const displayController = (() => {
+
+})();
+
+// Factory Function for Players
+const Player = () => {
+
+};
+
+const tiles = ['x', 'o', 'x', 'x', 'x', 'o', 'x', 'x', 'o', 'x'];
+const tile = document.querySelectorAll('.tile');
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', (tile) => {
+    for (let i = 0; i < tile.length; i++){
+        const newContent = document.createTextNode(tiles[i]);
+        tile.appendChild(newContent);
+    }
+});
