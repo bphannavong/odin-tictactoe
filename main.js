05d1900192fe5c7bb295a7795b3f59a9c7d9f8cb
Billy Phannavong <billy.phannavong@gmail.com>
2022-08-04 15:04:55 -0700

    Add some logic for clear board, need figure out e

--- main.js
+++ main.js
@@ -1,33 +1,58 @@
 // Gameboard object module
 const gameBoard = (() => {
 
-    let tiles = ['x', 'o', 'x', 'x', 'x', 'o', 'x', 'x', 'o'];
+    let tiles = new Array(9).fill(' '); //create tiles with space strings
+    const display = document.querySelector('.display');
 
+    function clear() {
+        display.innerHTML = ''; //clear board
+    }
 
-    return {tiles};
-})();
+    function renderBoard() {
+        let counter = 0;
+        clear();
 
-// Module object to control game flow
-const displayController = (() => {
-
-    function renderBoard(tilesArray) {
-        const display = document.querySelector('.display');
-        for (tile of tilesArray) {
+        for (tile of tiles) {
             const newDiv = document.createElement('div');
             const newContent = document.createTextNode(tile);
+
             newDiv.classList.add('tile');
+            newDiv.setAttribute('data-index', counter++);
             newDiv.appendChild(newContent);
             display.appendChild(newDiv);
         }        
     }
+    
+    function addTile() {
 
-    return {renderBoard};
+    }
+
+
+    return {renderBoard, clear};
+})();
+
+// Module object to control game flow
+const game = ((e) => {
+
+    const display = document.querySelector('.display');
+    const tiles = document.querySelectorAll('.tile');
+
+    console.log(e.target);
+    // if (e.target.classList == 'tile') {
+    //     e.target.textContent = 'x';
+        
+    // }
+    
+
 })();
 
 // Factory Function for Players
-const Player = () => {
-
+const Player = (name) => {
+    return {name};
 };
 
 const btn = document.querySelector('button');
-btn.addEventListener('click', displayController.renderBoard(gameBoard.tiles));
+// btn.addEventListener('click', displayController.renderBoard(gameBoard.tiles));
+btn.addEventListener('click', gameBoard.clear);
+btn.addEventListener('click', game(e)); //left off here
+gameBoard.renderBoard();
