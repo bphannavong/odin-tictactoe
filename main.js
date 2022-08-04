2248cb1829b20d6ac0e386ce3870a7be35f8663f
Billy Phannavong <billy.phannavong@gmail.com>
2022-08-04 13:16:33 -0700

    Able to renderBoard,need to prevent instant render

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 // Gameboard object module
 const gameBoard = (() => {
 
-    const tiles = ['x', 'o', 'x'];
+    let tiles = ['x', 'o', 'x', 'x', 'x', 'o', 'x', 'x', 'o'];
 
+
+    return {tiles};
 })();
 
 // Module object to control game flow
 const displayController = (() => {
 
+    function renderBoard(tilesArray) {
+        const display = document.querySelector('.display');
+        for (tile of tilesArray) {
+            const newDiv = document.createElement('div');
+            const newContent = document.createTextNode(tile);
+            newDiv.classList.add('tile');
+            newDiv.appendChild(newContent);
+            display.appendChild(newDiv);
+        }        
+    }
+
+    return {renderBoard};
 })();
 
 // Factory Function for Players
@@ -15,13 +29,5 @@ const Player = () => {
 
 };
 
-const tiles = ['x', 'o', 'x', 'x', 'x', 'o', 'x', 'x', 'o', 'x'];
-const tile = document.querySelectorAll('.tile');
-
 const btn = document.querySelector('button');
-btn.addEventListener('click', (tile) => {
-    for (let i = 0; i < tile.length; i++){
-        const newContent = document.createTextNode(tiles[i]);
-        tile.appendChild(newContent);
-    }
-});
+btn.addEventListener('click', displayController.renderBoard(gameBoard.tiles));
